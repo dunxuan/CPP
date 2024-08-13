467a3d85bfbc177a222e20bea441d4a2b3ce6cd5
dunxuan <dunxuan@dunxuan.xyz>
2024-08-13 22:14:15 +0800

    deleted:    luogu/learn/0/P1000.cpp 	deleted:    luogu/learn/0/P1001.cpp 	deleted:    luogu/learn/0/P1008.cpp 	deleted:    luogu/learn/1/1/P1014.cpp 	deleted:    luogu/learn/1/1/P1035.cpp 	deleted:    luogu/learn/1/1/P1085.cpp 	deleted:    luogu/learn/1/1/P1089.cpp 	deleted:    luogu/learn/1/1/P1089_6.in 	deleted:    luogu/learn/1/1/P1089_6.out 	deleted:    luogu/learn/1/1/P1307.cpp 	deleted:    luogu/learn/1/1/P1421.cpp 	deleted:    luogu/learn/1/1/P1909.cpp 	deleted:    luogu/learn/1/1/P1980.cpp 	deleted:    luogu/learn/1/2/P1046.cpp 	deleted:    luogu/learn/1/2/P1047.cpp 	deleted:    luogu/learn/1/2/P1047_4.in 	deleted:    luogu/learn/1/2/P1047_4.out 	deleted:    luogu/learn/1/2/P1427.cpp 	deleted:    luogu/learn/1/2/P2141.cpp 	deleted:    luogu/learn/1/2/P5594.cpp 	deleted:    luogu/learn/1/3/P1012.cpp 	deleted:    luogu/learn/1/3/P1055.cpp 	deleted:    luogu/learn/1/3/P1308.cpp 	deleted:    luogu/learn/1/3/P2010.cpp 	deleted:    luogu/learn/1/3/P2010_7.in 	deleted:    luogu/learn/1/3/P2010_7.out 	deleted:    luogu/learn/1/3/P2010_8.in 	deleted:    luogu/learn/1/3/P2010_8.out 	deleted:    luogu/learn/1/3/P5015.cpp 	deleted:    luogu/learn/1/3/P5587.cpp 	deleted:    luogu/learn/1/4/P1025.cpp 	deleted:    luogu/learn/1/4/P1028.cpp 	deleted:    luogu/learn/1/4/P1036.cpp 	deleted:    luogu/learn/1/4/P1192.cpp 	deleted:    luogu/learn/1/4/P1192_3.in 	deleted:    luogu/learn/1/4/P1192_3.out 	deleted:    luogu/learn/1/4/P1464.cpp 	deleted:    luogu/learn/1/4/P1464_3.in 	deleted:    luogu/learn/1/4/P1464_3.out 	deleted:    luogu/learn/1/4/P4994.cpp 	deleted:    luogu/learn/1/4/P5534.cpp 	deleted:    luogu/learn/2/1/P1003.cpp 	deleted:    luogu/learn/2/1/P1042.cpp 	deleted:    luogu/learn/2/1/P1067.cpp 	deleted:    luogu/learn/2/1/P1067_4.in 	deleted:    luogu/learn/2/1/P1067_4.out 	deleted:    luogu/learn/2/1/P1179.cpp 	deleted:    luogu/learn/2/1/P1328.cpp 	deleted:    luogu/learn/2/1/P1563.cpp 	deleted:    luogu/learn/2/1/P1563_1.in 	deleted:    luogu/learn/2/1/P1563_1.out 	deleted:    luogu/learn/2/1/P2482.cpp 	deleted:    luogu/learn/2/1/P2615.cpp 	deleted:    luogu/learn/2/1/P3952.cpp 	deleted:    luogu/learn/2/2/P1051.cpp 	deleted:    luogu/learn/2/2/P1051_2.in 	deleted:    luogu/learn/2/2/P1051_2.out 	deleted:    luogu/learn/2/2/P1059.cpp 	deleted:    luogu/learn/2/2/P1059_2.in 	deleted:    luogu/learn/2/2/P1059_2.out 	deleted:    luogu/learn/2/2/P1068.cpp 	deleted:    luogu/learn/2/2/P1068_1.in 	deleted:    luogu/learn/2/2/P1068_1.out 	deleted:    luogu/learn/2/2/P1177.cpp 	deleted:    luogu/learn/2/2/P1309.cpp 	deleted:    luogu/learn/2/2/P1309_2.in 	deleted:    luogu/learn/2/2/P1309_2.out 	deleted:    luogu/learn/2/2/P1908.cpp 	deleted:    luogu/learn/2/3/P1024.cpp 	deleted:    luogu/learn/2/3/P2678.cpp 	deleted:    luogu/learn/4/2/P1048_1.in 	deleted:    luogu/learn/4/2/P1048_1.out

--- luogu/learn/1/1/P1421.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-	int a, b;
-	std::cin >> a >> b;
-	b += a * 10;
-	std::cout << b / 19;
-}
--- luogu/learn/1/4/P5534.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main()
-{
-	long long a1, a2, n;
-	std::cin >> a1 >> a2 >> n;
-	long long sn = n * ((2 * a1) + (n - 1) * (a2 - a1)) / 2;
-	std::cout << sn;
-}
